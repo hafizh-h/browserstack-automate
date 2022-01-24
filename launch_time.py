28ce8c9539e88e627dc7ebbc561c7c33fd6c8147
Hafizh Hafizh <hafizhhafizh@Hafizh-Hafizh.local>
2022-01-24 11:42:45 +0700

    dump profiling & device logs

--- launch_time.py
+++ launch_time.py
@@ -3,6 +3,7 @@ import subprocess as sp
 import requests
 from requests.auth import HTTPBasicAuth
 import time
+import json
 '''
 Launch Time Comparison for v4.27.0 and v4.28.0
 App for v4.27.0 = v4.27.0-1-RC29-HEAD-131201-release.apk
@@ -45,5 +46,15 @@ driver.quit()
 
 basic = HTTPBasicAuth('hafizh_783gSd', 'cpKChBFWNYG4qaA4dj1H')
 time.sleep(30) #requests had to wait ~30s because BrowserStack took some time to generate the App Profilng Data
-r = requests.get('https://api.browserstack.com/app-automate/builds/0cf20a8dfbaa3208b41a29b0b9c03482664ccf02/sessions/' + str(session_id) + '/appprofiling', auth=basic)
-print(r.json())
+app_profiling = requests.get('https://api.browserstack.com/app-automate/builds/0cf20a8dfbaa3208b41a29b0b9c03482664ccf02/sessions/' + str(session_id) + '/appprofiling', auth=basic)
+app_start = requests.get('https://api.browserstack.com/app-automate/builds/0cf20a8dfbaa3208b41a29b0b9c03482664ccf02/sessions/' + str(session_id) + '/devicelogs', auth=basic)
+
+json_app_profiling = json.dumps(app_profiling.json(), indent=4, sort_keys=True)
+json_file = open(desired_caps["deviceName"]+" OS "+desired_caps["platformVersion"]+" performance.json", "w")
+json_file.write(json_app_profiling)
+json_file.close()
+
+app_start_file = open(desired_caps["deviceName"]+" OS "+desired_caps["platformVersion"]+" devicelogs.log", "w")
+app_start_file.write(app_start.text)
+app_start_file.close()
+
