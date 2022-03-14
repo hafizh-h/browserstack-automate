49a17c649bbcc86aed99852ee084e45130281126
Hafizh Hafizh <hafizhhafizh@Hafizh-Hafizh.local>
2022-03-14 14:18:35 +0700

    update url for 4.31.2 & 4.32.0

--- appium_device_log.py
+++ appium_device_log.py
@@ -1,12 +1,7 @@
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import subprocess as sp
-import requests
-from requests.auth import HTTPBasicAuth
-import time
-import json
+from selenium.webdriver.support import expected_conditions as ec
 '''
 Launch Time Comparison for v4.27.0 and v4.28.0
 App for v4.27.0 = v4.27.0-1-RC29-HEAD-131201-release.apk
@@ -18,59 +13,56 @@ App for v4.29.0 = v4.29.0-release-4.29.0-310104-debug.apk
 App url: bs://394ad5ded6384cd522fd1cf4bcfccfb19f73b16e
 App for v4.30.0 = v4.30.0-release-4.30.0-150206-release.apk
 App url: bs://e1cf27a39a6a15c3bec8fed938dcb71e707ea941
-
-Current Issues:
-- Can't get 'Displayed' and 'Fully drawn' from raw device logs
-- The metrics can't also be found in others Android build created
-  by other users
-TO-DO:
-- Check on how to get the metrics from the device logs in App Automate
-- Explore more about python subprocess.run methods
+App for v4.31.2 Release = 4b79495a-2741-4df1-8cd6-8cd2f58b630d.aab
+App url: bs://d74a6cff3ccc263ddc39e5e99cc45985dca71280
+App for v4.32.0 Release = 4a2d2f07-ec23-4f73-8f3e-4e232ee4a825.aab
+App url: bs://ea085709f45fb1e717328f916ac65f9703045736
 '''
 userName = "hafizh_783gSd"
 accessKey = "cpKChBFWNYG4qaA4dj1H"
 
 desired_caps = {
     "project": "Core Platform",
-    "build": "Launch Time 4.29.0 Debug",
+    "build": "Launch Time 4.31.2 Release",
     "name": "Samsung Galaxy S21",
     "platformName": "android",
     "platformVersion": "11.0",
     "deviceName": "Samsung Galaxy S21",
-    "browserstack.networkLogs": "true",
-    "app": "bs://394ad5ded6384cd522fd1cf4bcfccfb19f73b16e"
+    "app": "bs://d74a6cff3ccc263ddc39e5e99cc45985dca71280"
 }
 
 driver = webdriver.Remote("https://" + userName + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub", desired_caps)
 session_id = driver.session_id
+wait = WebDriverWait(driver, 10)
 
-btn_cancel_element = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((MobileBy.XPATH, "//*[contains(@resource-id, 'tds_btn') and (@text='Batalkan')]"))
+btn_cancel_element = wait.until(
+    ec.element_to_be_clickable((MobileBy.XPATH, "//*[contains(@resource-id, 'tds_btn') and (@text='Batalkan')]"))
 )
 btn_cancel_element.click()
 
-# btn_close_element = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((MobileBy.XPATH, "//android.view.View[@content-desc='light']/android.widget.Image"))
-# )
-# btn_close_element.click()
+btn_close_element = wait.until(
+    ec.element_to_be_clickable((MobileBy.XPATH, "//android.view.View[@content-desc='light']/android.widget.Image"))
+)
+btn_close_element.click()
 
-btn_login_menu_element = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((MobileBy.XPATH, "//*[contains(@resource-id, 'tds_title_bottom_navigation') and (@text='Masuk')]"))
+btn_login_menu_element = wait.until(
+    ec.element_to_be_clickable((MobileBy.XPATH, "//*[contains(@resource-id, 'tds_title_bottom_navigation') and ("
+                                                "@text='Masuk')]"))
 )
 btn_login_menu_element.click()
-time.sleep(45)
+
 logs = driver.get_log('logcat')
 log_messages = list(map(lambda log: log['message'], logs))
 driver.quit()
 
-log_file = open("device_log.txt", "w")
+log_file = open(desired_caps["deviceName"] + " OS "+desired_caps["platformVersion"] + " logs.txt", "w")
 for i in log_messages:
     log_file.write(i + "\n")
 log_file.close()
 
 perf_metrics = list(filter(lambda perf: 'I ActivityTaskManager' in perf, log_messages))
 
-perf_file = open("perf_log.txt", "w")
+perf_file = open(desired_caps["deviceName"] + " OS "+desired_caps["platformVersion"] + " perf_logs.txt", "w")
 for j in perf_metrics:
     perf_file.write(j + "\n")
 perf_file.close()
